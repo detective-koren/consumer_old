e25e24055de7579ae0710584b22d041b23c1aadb
Jongmyung Lee <bbb7931293@gist.ac.kr>
2019-09-22 18:11:03 +0900

    Using face recognition github code

--- image_capture.py
+++ image_capture.py
@@ -2,9 +2,9 @@ import cv2
 import numpy as np
 import time
 import importlib
+import face_recognition
 from os import listdir, remove
 from os.path import isfile, join
-from face_validation import face_recog
 # from datetime import datetime
 
 target = [f for f in listdir("./target") if isfile(join("./target", f))]
@@ -17,34 +17,66 @@ def image_capture(folder_name = "cropped", ms_itv = 100):
 
    '''
    video = cv2.VideoCapture(0)
+
+   known_face_encodings = []
+   known_face_names = []
+
+   for f in listdir("./target"):
+      target_image = face_recognition.load_image_file("./target/" + f)
+      target_face_encoding = face_recognition.face_encodings(target_image)[0]
+      known_face_encodings.append(target_face_encoding)
+      known_face_names.append(f)
+
    prev_time = int(round(time.time() * 1000))
-   face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
+   
    while True:
       cur_time = int(round(time.time() * 1000))
-      check, img = video.read()
-      gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-      gray = cv2.equalizeHist(gray)
+      ret, frame = video.read()
+      
+      small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+      rgb_frame = small_frame[:, :, ::-1]
 
-      faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-      for (x, y, w, h) in faces:
-         img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)   
-      cv2.imshow("CCTV1", img)
+      face_locations = face_recognition.face_locations(rgb_frame)
+      face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
 
-      if cur_time - prev_time >= ms_itv:
-         # Cropping face, distinguish different face, save it
-         # Or save img, other process crop the face, and so on.
-         prev_time = cur_time
-         i = 0
+      i = 1
+      face_names = []
+
+      for face_encoding in face_encodings:
+          
+          matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+          name = "Unknown"
+
+          face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+          best_match_index = np.argmin(face_distances)
+          if matches[best_match_index]:
+              name = known_face_names[best_match_index]
+
+          face_names.append(name)
+
+      for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
          
-         for (x, y, w, h) in faces:
-            faceimg = img[y:y + h, x:x + w]
+         top *= 4
+         right *= 4
+         bottom *= 4
+         left *= 4
+
+         # Draw a box around the face
+         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+         # Draw a label with a name below the face
+         cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+         font = cv2.FONT_HERSHEY_DUPLEX
+         cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+      
+         if cur_time - prev_time >= ms_itv:
+            faceimg = small_frame[top: top + (top + bottom) // 2, left: left + (left + right) // 2]
             filename = "./" + folder_name + "/%d_%d.jpeg" %(cur_time, i)
             cv2.imwrite(filename, faceimg)
             i += 1
-            for t in target:
-               face_recog("./target/" + t, filename)
-               remove(filename)
         
+      cv2.imshow("CCTV1", frame)
+      
       if cv2.waitKey(1) & 0xFF == ord('q'):
          break;
 
